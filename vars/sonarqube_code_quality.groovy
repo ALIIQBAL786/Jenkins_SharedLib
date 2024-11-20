373410a9c095d205a0f5ac8e910ceadf0158d137
Ali Iqbal <97777690+ALIIQBAL786@users.noreply.github.com>
2024-11-20 19:35:25 +0500

    Update sonarqube_code_quality.groovy

--- vars/sonarqube_code_quality.groovy
+++ vars/sonarqube_code_quality.groovy
@@ -1,5 +1,6 @@
-def call(){
-  timeout(time: 5, unit: "MINUTES"){
-      waitForQualityGate abortPipeline: false
-  }
+def call() {
+    timeout(time: 5, unit: "MINUTES") {
+        def qualityGate = waitForQualityGate abortPipeline: false
+        echo "SonarQube Quality Gate status: ${qualityGate.status}"
+    }
 }
